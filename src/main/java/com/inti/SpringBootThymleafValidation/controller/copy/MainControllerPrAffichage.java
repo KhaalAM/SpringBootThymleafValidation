dafab386b095926cdcd66c28612119b998f822d1
KhaalAM <au.mollier@gmail.com>
2022-05-23 10:37:18 +0200

    commit 2 ajout de la classe utilisateur et annotations

--- src/main/java/com/inti/SpringBootThymleafValidation/controller/copy/MainControllerPrAffichage.java
+++ src/main/java/com/inti/SpringBootThymleafValidation/controller/copy/MainControllerPrAffichage.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainControllerPrAffichage {
 
 	
-	@GetMapping("hello")
+	@GetMapping("/hello")
 	public String hello()
 	{
 		return "hello";
